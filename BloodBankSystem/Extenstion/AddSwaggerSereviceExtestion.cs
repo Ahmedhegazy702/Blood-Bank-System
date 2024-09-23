8edc53139d9c912f5ff9ca1051c45629ea90ea1e
Ahmed Ibrahim <120848252+Ahmedhegazy702@users.noreply.github.com>
2024-09-23 18:43:35 +0300

    sys02

---
+++ BloodBankSystem/Extenstion/AddSwaggerSereviceExtestion.cs
@@ -0,0 +1,24 @@
+﻿namespace BloodBankSystem.Extenstion
+{
+    public static class AddSwaggerSereviceExtestion
+    {
+        public static IServiceCollection AddSwaggerServices(this IServiceCollection services)
+        {
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            return services;
+        }
+
+
+        public static WebApplication UseSwaggerMiddleWares(this WebApplication app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+            return app;
+        }
+    }
+}
+
---
+++ BloodBankSystem/Extenstion/IdentityServicesExtenstion.cs
@@ -0,0 +1,63 @@
+﻿using BloodBank.Core.Entites.Identity;
+using BloodBank.Core.Services;
+using BloodBank.Repository.Identity;
+using BloodBank.Service;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
+
+namespace BloodBankSystem.Extenstion
+{
+    public static class IdentityServicesExtenstion
+    {
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped(typeof(ITokenService), typeof(TokenService));
+            services.AddAuthentication();
+            
+
+
+            services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<AppIdentityDbContext>();/*(options =>*/
+            //{
+            //    //options.Password.RequiredUniqueChars = 2;
+            //    //options.Password.RequireNonAlphanumeric = true;
+            //    //options.Password.RequireUppercase = true;
+            //    //options.Password.RequireLowercase = true;
+            //}).AddEntityFrameworkStores<AppIdentityDbContext>();
+
+            services.AddAuthentication(/*JwtBearerDefaults.AuthenticationScheme*/ options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+
+
+                        ValidateIssuer = true,
+                        ValidIssuer = configuration["JWT:ValidIssuer"],
+                        ValidateAudience = true,
+                        ValidAudience = configuration["JWT:ValidAudience"],
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:SecretKey"])),
+                        ClockSkew = TimeSpan.FromDays(double.Parse(configuration["JWT:DurationInDays"])),
+                        //claims: authClaims,
+                       
+
+                    };
+                });
+                //.AddJwtBearer("Bearer02", options =>
+                //{
+
+                //});
+
+            return services;
+        }
+    }
+}
