84e7ef8b864d656e73c37adf71af4fe489c0b9e0
Ahmed Ibrahim <120848252+Ahmedhegazy702@users.noreply.github.com>
2024-09-23 17:08:10 +0300

    Save Service

---
+++ BloodBank.Service/MailboxEmailVerification.cs
@@ -0,0 +1,34 @@
+﻿using BloodBank.Core.Services;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace BloodBank.Service
+{
+    public class MailboxEmailVerification : IEmailVerificationService
+    {
+        private readonly HttpClient _httpClient;
+
+        public MailboxEmailVerification(HttpClient httpClient)
+        {
+           _httpClient = httpClient;
+        }
+        public async Task<bool> IsValidEmailAsync(string email)
+        {
+            var response = await _httpClient.GetAsync($"http://apilayer.net/api/check?access_key=YOUR_ACCESS_KEY&email={email}$smtp=1&format=1");
+            var content = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<MailboxLayerResponse>(content);
+            return result.IsValidEmail;
+        }
+        private class MailboxLayerResponse
+        {
+            [JsonProperty("valid")]
+            public bool IsValidEmail { get; set; }
+
+        }
+    }
+}
---
+++ BloodBank.Service/TokenService.cs
@@ -0,0 +1,56 @@
+﻿using BloodBank.Core.Entites.Identity;
+using BloodBank.Core.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodBank.Service
+{
+    public class TokenService : ITokenService
+    {
+        private readonly IConfiguration _configuration;
+
+        public TokenService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public async Task<string> CreateTokenAsync(AppUser user, UserManager<AppUser> userManager)
+        {
+            // Private Claims [User-Defined]
+            var authClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.GivenName,user.UserName),
+                new Claim(ClaimTypes.Email,user.Email)
+            };
+            var userRoles = await userManager.GetRolesAsync(user);
+            foreach (var role in userRoles)
+            {
+                authClaims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            var authKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:SecretKey"]));
+
+            var token = new JwtSecurityToken(
+
+                 // Registered Claims
+                 issuer: _configuration["JWT:ValidIssuer"],
+                audience: _configuration["JWT:ValidAudience"],
+                expires: DateTime.UtcNow.AddDays(double.Parse(_configuration["JWT:DurationInDays"])),
+                // Private Claims
+                claims: authClaims,
+                // Key + Security Algorithm
+                signingCredentials: new SigningCredentials(authKey, SecurityAlgorithms.HmacSha256Signature)
+                );
+
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
